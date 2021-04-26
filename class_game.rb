ed1aba7f92e9d98e6cbcf38f8c537a6f1927711c
Gabriel Silva <j.gabriel.888@gmail.com>
2021-04-26 18:43:32 +0000

    player switcher method

--- class_game.rb
+++ class_game.rb
@@ -10,6 +10,14 @@ class Game
     @current_player = @p1
   end
 
+  def player_switcher()
+    if @current_player.name == "Player 1"
+      @current_player = @p2
+    else
+      @current_player = @p1
+    end
+  end
+
   def start_game
     while @p1.lives > 0 && @p2.lives > 0 do
       puts "---------------------------------------"
@@ -23,16 +31,14 @@ class Game
         puts "Congratulations #{@current_player.name} answered correctly!!!"
       else 
         @current_player.lose_life()
-        puts "Wrong answer you have #{@current_player.lives} lifes"
+        puts "Wrong answer you have #{@current_player.lives} lives"
       end
-      if @current_player.name == "Player 1"
-        @current_player = @p2
-      else
-        @current_player = @p1
-      end
+      player_switcher()
     end
-    puts "Game Over! #{@current_player.name} lost all lifes."
+      
+    puts "Game Over! #{@current_player.name} lost all lives."
+    player_switcher()
+    puts "CONGRATULATIONS #{@current_player.name} YOU ARE THE WINNER!"
   end
-
 end
 
